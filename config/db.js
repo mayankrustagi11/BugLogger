dde212f2ef40c21722d87bbb12e87c6d800efafc
Mayank Rustagi <marustagi@deloitte.com>
2020-06-07 19:07:03 +0530

    BugLogger

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://mayank:mayank123@buglogger-b0t4d.mongodb.net/buglogger?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB Connected')
+    } catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
